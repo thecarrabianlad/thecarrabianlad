77273a35253a21d6a0b837aa33f5cdb67153f4cf
thecarrabianlad <sarthak.2803.main@gmail.com>
2025-05-15 03:25:05 +0530

    three js library and other work

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 // Create the scene
@@ -6,12 +6,12 @@ const scene = new THREE.Scene();
 
 // Set up the camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+camera.position.z = 15;
 
 // Set up the renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+document.getElementById('container3D').appendChild(renderer.domElement);
 
 // Add a light source
 const light = new THREE.DirectionalLight(0xffffff, 1);
@@ -20,11 +20,11 @@ scene.add(light);
 
 // Load the 3D object
 const loader = new GLTFLoader();
-loader.load(
-  '../models/pirate-ship.glb', // Path to your 3D model
-  (gltf) => {
-    const model = gltf.scene;
-    scene.add(model);
+let ship;
+loader.load('../models/pirate-ship.glb', // Path to your 3D model
+  function(gltf) {
+    ship = gltf.scene;
+    scene.add(model); 
   },
   undefined,
   (error) => {
